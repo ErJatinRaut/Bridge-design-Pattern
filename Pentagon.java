bed39f532021cd55c383ca77599cf49da6745166
ErJatinRaut <104989023+ErJatinRaut@users.noreply.github.com>
2022-05-05 18:02:39 +0530

    Add files via upload

---
+++ Pentagon.java
@@ -0,0 +1,12 @@
+public class Pentagon extends Shape{
+
+	public Pentagon(Color c) {
+		super(c);
+	}
+
+	public void applyColor() {
+		System.out.print("Pentagon filled with color ");
+		color.applyColor();
+	} 
+
+}
---
+++ Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle extends Shape{
+
+	public Triangle(Color c) {
+		super(c);
+	}
+
+	public void applyColor() {
+		System.out.print("Triangle filled with color.. ");
+		color.applyColor();
+	} 
+
+}
